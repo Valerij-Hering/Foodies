6a0d7fb1b37cea5bcd5802c37a84331ac3372d6d
“Valerij-Hering” <zell9113@gmail.com>
2023-02-09 13:12:31 +0100

    first commit

--- src/Components/Filter/AllCategories.js
+++ src/Components/Filter/AllCategories.js
@@ -4,8 +4,7 @@ import Filter from "./Filter"
 const AllCategories = () => {
     return (
         <div className="cont">
-            {['Breakfast', 'Lunch', 'Dinner', 'All'].
-            map((category, index) => <Filter category={category} key={index}/>)}
+            {['Breakfast', 'Lunch', 'Dinner', 'All'].map((category, index) => <Filter category={category} key={index}/>)}
             
         </div>
     )
